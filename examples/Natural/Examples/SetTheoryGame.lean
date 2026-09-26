4932fcd7382ba11767abb6533b4e52b93b8ce9c0
Adam Dingle <adam@medovina.org>
2026-09-26 19:25:14 +0200

    Add more simple set theorems

--- examples/Natural/Examples/SetTheoryGame.lean
+++ examples/Natural/Examples/SetTheoryGame.lean
@@ -20,8 +20,24 @@ Theorem.  Let A, B, C : Set(T).
 
 -- intersection world
 
-Theorem.  Let A, B : Set(T).
+Theorem.  Let A, B, C : Set(T).
 
   a. Let x : T.  Suppose that x ∈ A and x ∈ B.  Then x ∈ A.
 
   b. Suppose that x ∈ A ∩ B.  Then x ∈ B.
+
+  c. A ∩ B ⊆ A.
+
+  d. Let x : T.  Suppose that x ∈ A and x ∈ B.  Then x ∈ A ∩ B.
+
+  e. Suppose that A ⊆ B and A ⊆ C.  Then A ⊆ B ∩ C.
+
+  f. A ∩ B ⊆ B ∩ A.
+
+  g. A ∩ B = B ∩ A.
+
+  h. (A ∩ B) ∩ C = A ∩ (B ∩ C).
+
+Proof.
+
+  h. Let A, B, C : Set(T). Let x : T.  Suppose that x ∈ (A ∩ B) ∩ C.  Then x ∈ A ∩ (B ∩ C).  Conversely, suppose that x ∈ A ∩ (B ∩ C).  Then x ∈ (A ∩ B) ∩ C.
